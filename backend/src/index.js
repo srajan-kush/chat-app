eb89ddf13e07a7014567cc6b4584df5872dd5385
srajan-kush <144719261+srajan-kush@users.noreply.github.com>
2025-03-05 23:17:49 +0530

    jwt + signup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,20 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js"
 
+
+dotenv.config();
 const app = express();
+
+const PORT = process.env.PORT
+
+app.use(express.json());
 
 app.use("/api/auth",authRoutes);
 
 
-
-const PORT = 5001;
-app.listen(PORT,() => console.log(`Server Started at Port:${PORT}`));
+app.listen(PORT,() => {
+    console.log(`Server Started at Port:${PORT}`);
+    connectDB();
+});
