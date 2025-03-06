df6d7351876218b453c13ad8c641bfdf21fd567d
srajan-kush <144719261+srajan-kush@users.noreply.github.com>
2025-03-06 23:26:53 +0530

    frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,11 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
+import cors from "cors";
 
 import { connectDB } from "./lib/db.js"
 
@@ -14,8 +18,16 @@ const PORT = process.env.PORT
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors(
+    {
+        origin: "http://localhost:5173",
+        credentials: true,
+    }
+))
 
 app.use("/api/auth",authRoutes);
+app.use("/api/message",messageRoutes);
+
 
 
 app.listen(PORT,() => {
