ab3d803dcf0eb00407b2d0d4cba9fc0c4487d327
srajan-kush <144719261+srajan-kush@users.noreply.github.com>
2025-03-06 21:29:29 +0530

    authentication & Message model

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import { connectDB } from "./lib/db.js"
+
 
 
 dotenv.config();
@@ -10,6 +13,7 @@ const app = express();
 const PORT = process.env.PORT
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
 
