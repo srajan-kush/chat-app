51d3f8c3cfbc0976eaeaa0ee14dd2e442ba4ef54
srajan-kush <144719261+srajan-kush@users.noreply.github.com>
2025-03-12 15:50:54 +0530

    Update message.controller.js

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -54,18 +54,19 @@ export const sendMessages = async (req, res) => {
             image: imageUrl,
         });
 
-        await newMessage.save();
+        const savedMessage = await newMessage.save();
 
-        res.status(201).json(newMessage);
-
-        // todo: realtime functionality goes here => socket.io
+        // Send socket event before HTTP response
         const receiverSocketId = getReceiverSocketId(receiverId);
         if(receiverSocketId){
-            io.to(receiverSocketId).emit("newMessage",newMessage);
+            io.to(receiverSocketId).emit("newMessage", savedMessage);
         }
+
+        // Send HTTP response only once
+        return res.status(201).json(savedMessage);
 
     } catch (error) {
         console.log("Error in sendMessages controller: ", error.message);
-        res.status(500).json({ error: "Internal server error"}); 
+        return res.status(500).json({ error: "Internal server error"}); 
     }
 };
